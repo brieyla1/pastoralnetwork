195effb5688bcd35828b13112594b7b31740864c
Lancelot de Briey <business@lancedb.dev>
2023-03-04 23:22:32 -0700

    more changes

--- apps/web/src/components/Profile/Cover.tsx
+++ apps/web/src/components/Profile/Cover.tsx
@@ -1,32 +1,20 @@
 import imageProxy from '@lib/imageProxy';
-import { COVER, STATIC_IMAGES_URL } from 'data/constants';
-import Image from 'next/image';
+import { COVER } from 'data/constants';
 import type { FC } from 'react';
 import getIPFSLink from 'utils/getIPFSLink';
-import backgroundImage from 'public/assets/img/default-background.svg';
 
 interface Props {
   cover: string;
 }
 
 const Cover: FC<Props> = ({ cover }) => {
-  return cover && cover != `${STATIC_IMAGES_URL}/patterns/2.svg` ? (
+  return (
     <div
       className="text-brand-400 h-52 sm:h-80"
       style={{
         backgroundImage: `url(${
-          cover ? imageProxy(getIPFSLink(cover), COVER) : `${STATIC_IMAGES_URL}/patterns/2.svg`
+          cover ? imageProxy(getIPFSLink(cover), COVER) : `url("public/assets/img/default-background.svg")`
         })`,
-        backgroundSize: cover ? 'cover' : '30%',
-        backgroundPosition: 'center center',
-        backgroundRepeat: cover ? 'no-repeat' : 'repeat'
-      }}
-    />
-  ) : (
-    <div
-      className="text-brand-400 h-52 sm:h-80"
-      style={{
-        backgroundImage: `url(${backgroundImage.src})`,
         backgroundSize: cover ? 'cover' : '30%',
         backgroundPosition: 'center center',
         backgroundRepeat: cover ? 'no-repeat' : 'repeat'
